caf9f7cb0fd196655c2d7e7f9a4326d230603201
boyunjjang1 <boyunqueen@gmail.com>
2019-09-28 14:23:56 +0900

    ADD Photon Server

---
+++ Assets/PhotonInit.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonInit : MonoBehaviourPunCallbacks
+{
+    public string gameVersion = "1.0";
+    public string nickName = "Boyun";
+
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    void Start()
+    {
+        OnLogin();
+    }
+    
+    void OnLogin()
+    {
+        PhotonNetwork.GameVersion = this.gameVersion;
+        // 게임버전 설정, 같은 버전끼리 공유 됨
+        PhotonNetwork.NickName = this.nickName;
+        PhotonNetwork.ConnectUsingSettings(); 
+        // 포톤을 이용한 온라인 연결
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected !!!");
+        PhotonNetwork.JoinRandomRoom(); 
+        // 생성 룸 랜덤 접속
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed join Room!!");
+        this.CreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+        PhotonNetwork.Instantiate("Player", new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void CreateRoom()
+    {
+        //CreateRoom(방이름,방옵션)
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+    }
+
+
+}
---
+++ Assets/PlayerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class PlayerCtrl : MonoBehaviourPunCallbacks
+{
+    private float h, v, r;
+    private Transform tr;
+    public float speed = 10.0f;
+    public float rotSpeed = 60.0f;
+    
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+
+    }
+    void Update()
+    {
+        if(photonView.IsMine)
+        {
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
+            Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+            tr.Translate(moveDir * Time.deltaTime * speed);
+
+            r = Input.GetAxis("Mouse X");
+            tr.Rotate(Vector3.up * Time.deltaTime * r * rotSpeed);
+
+  
+
+        }
+        
+    }
+}
